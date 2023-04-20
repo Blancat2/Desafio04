1ca0cd5b3d062a6fb04a16ef2a9034f39a47305e
Agustin Blancat <agustinblancat@gmail.com>
2023-04-19 21:25:44 -0300

    terminado

---
+++ src/app/pages/pagina-dos/pagina-dos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pagina-dos-page',
+  templateUrl: './pagina-dos.component.html',
+  styleUrls: ['./pagina-dos.component.scss']
+})
+export class paginaDos {
+
+}
---
+++ src/app/pages/pagina-dos/pagina-dos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { paginaDosComponent } from './pagina-dos.component';
+
+
+
+@NgModule({
+  declarations: [
+    paginaDosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class paginaDosModule { }
